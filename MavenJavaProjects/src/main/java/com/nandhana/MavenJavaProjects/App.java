758474082201143e70e6988a40d433ba9debc14a
Nandhana18 <nandhanabaskar2000@gmail.com>
2023-02-13 03:20:57 -0800

    commit changes

---
+++ MavenJavaProjects/src/main/java/com/nandhana/MavenJavaProjects/App.java
@@ -0,0 +1,13 @@
+package com.nandhana.MavenJavaProjects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "WELCOME TO DEVOPS LEARNING -NANDHANA" );
+    }
+}
